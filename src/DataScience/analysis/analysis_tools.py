b6af934f41319fe5f82eb81141530307806c6c5e
LukasBauer <ge97joj@mytum.de>
2024-01-25 17:12:43 +0100

    copied generate data also in the analysis part as it seems to be a bit off in the description

--- src/DataScience/analysis/analysis_tools.py
+++ src/DataScience/analysis/analysis_tools.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import pandas as pd
+import numpy as np
+
 def analyse_data(raw_data: pd.DataFrame) -> pd.DataFrame:
     """Produce and analyse raw data.
 
@@ -19,3 +26,19 @@ def analyse_data(raw_data: pd.DataFrame) -> pd.DataFrame:
         pct84.append(np.percentile(raw_data['y'][idx],84))
         x_mean.append(np.mean(raw_data['x'][idx]))
     return pd.DataFrame({'pct16': pct16, 'pct84': pct84, 'x_mean': x_mean})
+
+
+def generate_data(n: int = 1000) -> pd.DataFrame:
+    """Generate data points.
+
+    Args:
+        n: Number of datapoints. Defaults to 1000.
+
+    Returns:
+        dataframe with raw data.
+    """
+    np.random.seed(42)
+    x = np.linspace(0,2500, n)
+    noise_component = np.random.rand(n)
+    y = (x + x*noise_component/3)
+    return pd.DataFrame({'x': x, 'y': y})
